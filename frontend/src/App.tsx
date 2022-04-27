51edee602825dde10ec857249c47c7663270f6d5
mohamedelserafy <elserafy02@gmail.com>
2022-04-27 11:44:35 +0200

    update home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NamesList from "./components/NamesList";
+import AddName from "./components/NamesForm";
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <NamesList />
+      <AddName />
     </div>
   );
 }
