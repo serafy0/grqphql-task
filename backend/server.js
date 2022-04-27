0bd6b56da801172253fc4c5b267dfe0a7ee1c998
mohamedelserafy <elserafy02@gmail.com>
2022-04-27 09:43:06 +0200

    setup graphql server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,21 @@ import express from "express";
 
 import cors from "cors";
 import helmet from "helmet";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./schema/schema.js";
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(8080, () => {
   console.log("now listening");
