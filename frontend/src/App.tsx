f60e6da6bef56b2651644c3a288e5e52a1ed3c03
mohamedelserafy <elserafy02@gmail.com>
2022-04-28 07:52:39 +0200

    remove unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import NamesList from "./components/NamesList";
-import AddName from "./components/NamesForm";
 
 function App() {
   return (
