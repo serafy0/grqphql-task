9d9de75ed7832393c0b20f7c1671a173e8401eca
mohamedelserafy <elserafy02@gmail.com>
2022-05-09 12:33:55 +0200

    setup mobile app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Blink } from "react-native";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import NamesList from "./components/NamesList";
+import { BACKEND_URL } from "@env";
+
+const client = new ApolloClient({
+  uri: BACKEND_URL,
+
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Names List</Text>
+        <NamesList />
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
